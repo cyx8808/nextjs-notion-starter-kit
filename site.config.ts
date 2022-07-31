27985c67ba73d68f7b998e63711518e164754266
Yuxiang <bigpig@outlook.com>
2022-07-31 15:56:39 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,12 +14,12 @@ export default siteConfig({
   author: 'Yuxiang',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  // description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -50,15 +50,15 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationStyle: 'custom',
+  navigationLinks: [
+  {
+      title: 'About',
+      pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+     },
+     {
+       title: 'Contact',
+       pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+     }
+   ]
 })
